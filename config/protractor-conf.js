21fea389b3148c57b69be549505f2593174ed09d
Paulo Franca <francaniilista@gmail.com>
2017-03-30 15:20:39 -0300

    git commit tests

--- config/protractor-conf.js
+++ config/protractor-conf.js
@@ -1,9 +1,8 @@
+var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 exports.config = {
 	allScriptsTimeout: 11000,
 
-	specs: [
-		'../test/e2e/*.js'
-	],
+	specs: ['../test/e2e/*.js'],
 
 	capabilities: {
 		'browserName': 'chrome'
@@ -14,6 +13,17 @@ exports.config = {
 	framework: 'jasmine',
 
 	jasmineNodeOpts: {
+		showColors: true,
 		defaultTimeoutInterval: 30000
+	},
+	
+	beforeLaunch: function() {
+		require('ts-node').register({
+			project: './test/e2e/'
+		});
+	},
+
+	onPrepare: function() {
+		jasmine.getEnv().addReporter(new SpecReporter());
 	}
 };
